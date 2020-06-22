e1e01a7a8fe227e4cd7afbea6c9f32f04ca800f6
Tamer Berat Çelik <aducat5@hotmail.com>
2020-06-22 23:52:07 +0300

    login, register and forgot pages added, other pages removed.

--- PMCMS.PL/Controllers/HomeController.cs
+++ PMCMS.PL/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using PMCMS.BSL.Authorization;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,22 +9,13 @@ namespace PMCMS.PL.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+        [UserAuth]
+        public ActionResult Dashboard()
         {
             return View();
         }
-
-        public ActionResult About()
+        public ActionResult Landing()
         {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
             return View();
         }
     }
---
+++ PMCMS.PL/Controllers/SignController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PMCMS.PL.Controllers
+{
+    public class SignController : Controller
+    {
+        // GET: Sign
+        public ActionResult Up()
+        {
+            return View();
+        }
+        
+        public ActionResult In()
+        {
+            return View();
+        }
+        public ActionResult Recover()
+        {
+            return View();
+        }
+
+    }
+}
