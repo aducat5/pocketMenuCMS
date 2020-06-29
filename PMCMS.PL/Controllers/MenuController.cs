d81b65b1d1df2fe8c593141e64ffbb0277ba4777
Tamer Berat Çelik <aducat5@hotmail.com>
2020-06-29 22:53:59 +0300

    added custom errors and display screen

--- PMCMS.PL/Controllers/MenuController.cs
+++ PMCMS.PL/Controllers/MenuController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using PMCMS.BLL.Repos;
+using PMCMS.DAL;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -16,7 +18,9 @@ namespace PMCMS.PL.Controllers
 
         public ActionResult Display(int id)
         {
-            return View(id);
+            SellerRepo sr = new SellerRepo();
+            Seller seller = sr.GetSeller(id);
+            return View(seller);
         }
 
         public ActionResult Edit()
