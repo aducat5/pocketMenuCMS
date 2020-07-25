6f5051bd2ed411d4e1e401b1ad2a0caea7c02912
Tamer Berat Çelik <aducat5@hotmail.com>
2020-07-25 11:27:28 +0300

    changed route to landing for opening landing as default

--- PMCMS.PL/App_Start/RouteConfig.cs
+++ PMCMS.PL/App_Start/RouteConfig.cs
@@ -23,7 +23,7 @@ namespace PMCMS.PL
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Dashboard", id = UrlParameter.Optional }
+                defaults: new { controller = "Home", action = "Landing", id = UrlParameter.Optional }
             );
 
         }
